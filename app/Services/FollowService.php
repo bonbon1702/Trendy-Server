768af8f7d52a6957b5117cf7eb810246732683e9
NamcoolNguyen <namndse02613@fpt.edu.vn>
2015-01-21 20:45:42 +0700

    update route

--- app/Services/FollowService.php
+++ app/Services/FollowService.php
@@ -25,15 +25,38 @@ class FollowService implements BaseService{
     public function create(array $data)
     {
         // TODO: Implement create() method.
+        $userId=$data['user_id'];
+        $followerId=$data['follower_id'];
+        $this->followRepository->create(array(
+            'user_id'=>$userId,
+            'follower_id'=>$followerId,
+        ));
+
     }
 
     public function update(array $data)
     {
         // TODO: Implement update() method.
+        $this->followRepository->update('id',$data['id'],$data);
     }
 
     public function delete($column, $value)
     {
         // TODO: Implement delete() method.
+        return $this->followRepository->deleteWhere($column,$value);
+    }
+
+    public function FollowingByUser()
+    {
+        // TODO: Implement delete() method.
+       // $user_id = $this->userRepository->getRecent()->id;
+
+        return $this->followRepository->getWhere('user_id',7)->get();
+    }
+
+    public function FollowerByUser(){
+
+        // TODO: Implement delete() method.
+        return $this->followRepository->getWhere('follower_id',1)->get();
     }
 }
