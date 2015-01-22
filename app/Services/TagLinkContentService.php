b0cf34972af73f9a5ec76d90ec754e9f7ad4527d
HacLongKym <haclongkim9x@gmail.com>
2015-01-22 18:30:09 +0700

    luan: album implement

--- app/Services/TagLinkContentService.php
+++ app/Services/TagLinkContentService.php
@@ -15,25 +15,44 @@ use Repositories\TagLinkContentRepository;
 class TagLinkContentService implements BaseService{
 
     private $tagLinkContentRepository;
-    function __construct(TagLinkContentRepository $tagLinkContentRepository)
+    private $tagLinkRepository;
+    function __construct(TagLinkContentRepository $tagLinkContentRepository,TagLinkRepository $tagLinkRepository)
     {
         // TODO: Implement __construct() method.
         $this->tagLinkContentRepository = $tagLinkContentRepository;
+        $this->tagLinkRepository = $tagLinkRepository;
     }
 
     public function create(array $data)
     {
-        // TODO: Implement create() method.
+        $tag_id = $data['tag_id'];
+        $tag_content_id = $data['tag_content_id'];
+        $post = $this->tagLinkRepository->get($tag_id);
+        if ($post) {
+            $this->tagLinkContentRepository->create(array(
+                'tag_id' => $tag_id,
+                'tag_content_id' => $tag_content_id,
+            ));
+        }
+        return true;
     }
 
     public function update(array $data)
     {
-        // TODO: Implement update() method.
+        $column = $data['column'];
+        $value = $data['value'];
+        $tag_id = $data['tag_id'];
+        $tag_content_id = $data['tag_content_id'];
+        $this->albumRepository->update($column, $value, array(
+            'tag_id' => $tag_id,
+            'tag_content_id' => $tag_content_id,));
+
     }
 
     public function delete($column, $value)
     {
         // TODO: Implement delete() method.
+        $this->albumRepository->delete($column,$value);
     }
 
 }
