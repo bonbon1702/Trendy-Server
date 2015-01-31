ff4c72a951ebf4a4a80be94e5dbaecdec9e7ef7a
Nghiavt <tuannghia1702@gmail.com>
2015-01-31 17:31:31 +0700

    Nghia: fix

--- app/models/User.php
+++ app/models/User.php
@@ -31,6 +31,24 @@ class User extends \Eloquent implements UserInterface, RemindableInterface
     {
         return $this->getKey();
     }
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+    /**
+     * Get the e-mail address where password reminders are sent.
+     *
+     * @return string
+     */
+    public function getReminderEmail()
+    {
+        return $this->email;
+    }
 
     public function role()
     {
