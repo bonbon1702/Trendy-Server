06b5057497912e647b0c8ef8897dedab584454c1
NamcoolNguyen <namndse02613@fpt.edu.vn>
2015-03-04 15:46:21 +0700

    update PHPDoc Comments

--- app/Services/AlbumService.php
+++ app/Services/AlbumService.php
@@ -15,17 +15,39 @@ use Repositories\PostRepository;
 use Repositories\UserRepository;
 use Repositories\PostAlbumRepository;
 
+/**
+ * Class AlbumService
+ * @package Services
+ */
 class AlbumService implements BaseService
 {
 
+    /**
+     * @var AlbumRepository
+     */
     private $albumRepository;
 
+    /**
+     * @var PostRepository
+     */
     private $postRepository;
 
+    /**
+     * @var UserRepository
+     */
     private $userRepository;
 
+    /**
+     * @var PostAlbumRepository
+     */
     private $postAlbumRepository;
 
+    /**
+     * @param AlbumRepository $albumRepository
+     * @param PostRepository $postRepository
+     * @param UserRepository $userRepository
+     * @param PostAlbumRepository $postAlbumRepository
+     */
     function __construct(AlbumRepository $albumRepository, PostRepository $postRepository, UserRepository $userRepository,PostAlbumRepository $postAlbumRepository)
     {
         // TODO: Implement __construct() method.
@@ -35,6 +57,10 @@ class AlbumService implements BaseService
         $this->postAlbumRepository = $postAlbumRepository;
     }
 
+    /**
+     * @param array $data
+     * @return mixed
+     */
     public function create(array $data)
     {
         // TODO: Implement create() method.
@@ -47,6 +73,10 @@ class AlbumService implements BaseService
         return $album;
     }
 
+    /**
+     * @param array $data
+     * @return bool
+     */
     public function update(array $data)
     {
         // TODO: Implement update() method.
@@ -54,17 +84,30 @@ class AlbumService implements BaseService
         return true;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     */
     public function delete($column, $value)
     {
         // TODO: Implement delete() method.
         $this->albumRepository->deleteWhere($column, $value);
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function getListAlbumOfUser($id)
     {
         return $this->albumRepository->getAlbumOfUser($id);
     }
 
+    /**
+     * @param $album_name
+     * @param $user_id
+     * @return mixed
+     */
     public function getAlbumDetail($album_name,$user_id)
     {
         return $this->albumRepository->joinPostAndAlbumAndPostAlbum()
@@ -74,6 +117,10 @@ class AlbumService implements BaseService
                                                     ->get();
     }
 
+    /**
+     * @param $userId
+     * @return mixed
+     */
     public function getAlbum($userId)
     {
         return $this->albumRepository->getRecent()
@@ -81,6 +128,9 @@ class AlbumService implements BaseService
                                             ->groupBy('album_name')->get();
     }
 
+    /**
+     * @param $album_name
+     */
     public function deleteAlbum($album_name)
     {
         $albums =$this->albumRepository->joinPostAndAlbumAndPostAlbum()
