237359e7ae87a36d6b57ece5b3af0c07ac47fc5d
bonbon1702 <tuannghia1702@gmail.com>
2015-04-30 22:09:42 +0700

    Nghia: fix

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -483,7 +483,7 @@ class PostService implements IPostService{
      */
     public function deletePost($id)
     {
-//        $this->postRepository->delete($id);
+        $this->postRepository->delete($id);
 //        $this->postAlbumRepository->getRecent()->where('post_id', $id)->delete();
 //        $this->commentService->deleteCommentInPost($id);
 //        $this->favoriteService->deleteFavoriteInPost($id);
